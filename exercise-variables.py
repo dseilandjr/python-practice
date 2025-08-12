22c994a9bd0cee0380fbf3e7a6f07c39e2bed9e2
Darrell Eiland Jr <dseilandjr@gmail.com>
2025-08-12 17:29:16 -0400

    initial commit

---
+++ exercise-variables.py
@@ -0,0 +1,55 @@
+#1 create variable onomatopeia and print the type
+onomatopoeia = "pop"
+print(type(onomatopoeia))
+
+#2 create variable lyrics assigned to multiline string
+lyrics = """Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:" """
+
+#3 create variable gpa print variable and type
+gpa = 3.75
+print(gpa)
+print(type(gpa))
+
+#4 create variable empty_var assinged to Python equivalent of null, print variable and type
+empty_var = None
+print(empty_var)
+print(type(empty_var))
+
+#5 create variable that stores 828^282 and print
+num = 828 ** 282
+print(num)
+
+#6 create variable to store result of (52-52) + (64/8) + the remainder of 42/8 and print
+num2 = ((52-52) + (64/8) + (42 % 8))
+print(num2)
+
+#7 create variable true, set to true, create second variabel isTrue to check is true is not False and print isTrue
+true = True
+isTrue = true != False
+print(isTrue)
+
+#8 create a variable weird_word, create a second variable check if matchs 'weird' and print second variable
+weird_word = "Weird"
+is_weird_word = weird_word == "weird"
+print(is_weird_word)
+
+#9 create variable named false
+false  # error because false is not defined "NameError: name 'false' is not defined. Did you mean: 'False'?"
+
+#10 create variable for value of 35/7, value of 15^3, value of variable 2 % variable 1, value of variable 2 / variable 1. Compare results
+num3 = 35/7
+num4 = 15 ** 3
+num5 = num4 % num3
+num6 = num4 / num3
+print(num3)
+print(num4)
+print(num5)
+print(num6)

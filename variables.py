7ba22b9101e23d8a9fcf9be395a0102799687c32
Darrell Eiland Jr <dseilandjr@gmail.com>
2025-08-12 17:06:38 -0400

    initial commit

---
+++ variables.py
@@ -0,0 +1,43 @@
+my_boolean = True
+my_int = 42
+my_float = 3.14159
+my_string = "Assess"
+my_string2 = "Educate"
+my_multi_line_string = """Assess
+Educate
+Advance"""
+
+print(my_boolean)
+print(my_multi_line_string)
+
+print(type(my_boolean))
+
+my_variable = 42
+print(type(my_variable))
+my_variable = 'test'
+print(type(my_variable))
+
+my_variable = None
+type(my_variable)
+print(type(my_variable))
+
+# Set up our variables
+num1 = 3.14
+num2 = 2
+num3 = 16.00
+# Create some expressions and store their results
+add_result = num1 + num2
+subtract_check = num2 - num1
+divide_check = num3 / num2
+exponent_result = num1 ** num2
+remainder_result = num3 % num2
+greater_than_check = num1 > num2
+multiple_ops_check = round((divide_check * 5) ** num1, 2)
+# Print the results
+print(add_result)
+print(subtract_check)
+print(divide_check)
+print(exponent_result)
+print(remainder_result)
+print(greater_than_check)
+print(multiple_ops_check)
